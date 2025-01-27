f4a6515380b7c26c3062e250059f080d5af71947
Vedant P <vpat1621@gmail.com>
2025-01-26 18:20:42 -0600

    commit as of 1/26

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
 
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
           <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
-            <p className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2">
+            <p className="bg-orange-100 text-orange-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-blue-400 mb-2">
               <svg
                 className="w-2.5 h-2.5 me-1.5"
                 aria-hidden="true"
@@ -23,30 +23,70 @@ function Projects() {
                   d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
                 />
               </svg>
-              Image Demo
+              Demo
             </p>
             <h1 className="text-gray-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-2">
-              Raspberry Pi OCR Summarizer
+              Amazon.com Product Analyzer
             </h1>
             <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-6">
-              Used a micro computer with a camera module to take low quality
-              images of text. Using image data from the camera, I was able to
-              extract characters, and feed them into a text summary pipeline.
-              This project called upon a variety of libraries, using
-              technologies in computer vision, machine learning, and natural
-              language processing.
+              Using Python, MySQL, and Natural Language Processing(NLP)
+              techniques, I created a script that analyzes reviews towards any
+              given Amazon.com product. My data came from a combination of two
+              customer review databases, totaling over 250 million
+              points of data. I heavily relied on SQL techniques like
+              aggregates, string functions, CTE's, and the Natural Language
+              Toolkit(NLTK) to optimize the program's efficiency.
             </p>
-            <a
-              href="https://docs.google.com/presentation/d/1np0eR8Sxbrn7FDmA1tCazrk8-DZt7iyXT6X9HTkIgq8/edit?usp=sharing"
-              className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            <button
+              className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-lg bg-orange-400 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Sample Images
-            </a>
+              Demo Coming Soon!
+            </button>
           </div>
 
           <div className="grid md:grid-cols-2 gap-8">
+            <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+              <p className="bg-yellow-100 text-yellow-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                <svg
+                  className="w-2.5 h-2.5 me-1.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 4 1 8l4 4m10-8 4 4-4 4M11 1 9 15"
+                  />
+                </svg>
+                Image Demo
+              </p>
+              <h2 className="text-gray-900 dark:text-white text-3xl font-extrabold mb-2">
+                Raspberry Pi OCR Summarizer
+              </h2>
+              <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+                Used a micro computer with a camera module to take low quality
+                images of text. Using image data from the camera, I was able to
+                extract characters, and feed them into a text summary pipeline.
+                This project called upon a variety of libraries, using
+                technologies in computer vision, machine learning, and natural
+                language processing.
+              </p>
+              <a
+                href="https://docs.google.com/presentation/d/1np0eR8Sxbrn7FDmA1tCazrk8-DZt7iyXT6X9HTkIgq8/edit?usp=sharing"
+                className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sample Images
+              </a>
+            </div>
+
             <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
               <p className="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
                 <svg
@@ -74,7 +114,8 @@ function Projects() {
                 friends. We utilized our skills in full-stack software
                 development to create websites for local businesses. This is a
                 venture that is ongoing, so we are continuing to look for
-                clients.
+                clients. We create our dynamic solutions leveraging React.js,
+                Tailwind CSS, and Vite.
               </p>
               <a
                 href="https://webscrapers.org/"
@@ -85,6 +126,7 @@ function Projects() {
                 webscrapers.org
               </a>
             </div>
+
             <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
               <a
                 href="https://docs.google.com/presentation/d/1C55xx0iuZxkgIDcGtdVzLzj6qlmxoCyT5WYhqj00Obw/edit#slide=id.p"
